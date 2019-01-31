75b4d710f7d119a5eb7ddbe6d2320d3f2d5fd892
deveshgautam-qss <deveshgautam@qsstechnosoft.com>
2019-01-31 16:25:05 +0530

    my cart

--- src/app/nowishop/pages/my-cart/my-cart.component.ts
+++ src/app/nowishop/pages/my-cart/my-cart.component.ts
@@ -8,8 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class MyCartComponent implements OnInit {
 
   constructor() { }
+  breedcrumb: any;
+  quantity: any = 1;
 
   ngOnInit() {
+  	this.showBreedcrumb();
+  }
+
+  showBreedcrumb(){
+    this.breedcrumb = [
+      {
+        "name": "Home",
+        "url": "/",
+        "active": false
+      },
+      {
+        "name": "My Cart",
+        "url": "#",
+        "active": true
+      }
+    ];
+  }
+
+  increaseQuantity(){
+    this.quantity += 1;
+  }
+
+  decreaseQuantity(){
+    if(this.quantity > 1){
+      this.quantity -= 1;
+    }
   }
 
 }
