748022276f3ecc5386247f80d28eea75dacd7528
devgautam170291 <devgautam.170291@gmail.com>
2019-02-14 23:54:36 +0530

    shipping and payment

--- src/app/nowishop/pages/shipping-payment/shipping-payment.component.ts
+++ src/app/nowishop/pages/shipping-payment/shipping-payment.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpService } from '../../../services/http.service';
+import { NowishopService } from '../../../services/nowishop.service';
+import { Router } from '@angular/router';
+declare let $: any;
 
 @Component({
   selector: 'app-shipping-payment',
   templateUrl: './shipping-payment.component.html',
-  styleUrls: ['./shipping-payment.component.css']
+  styleUrls: ['./shipping-payment.component.css', '../my-cart/my-cart.component.css']
 })
 export class ShippingPaymentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, 
+    private dataService: HttpService,
+    private nowishopGlobal: NowishopService) { }
+
+  paymentProgress: any;
+  breedcrumb: any;
+  cartList: any = [];
 
   ngOnInit() {
+  	this.loadModel();
+  	this.showBreedcrumb();
+  	this.getMyCart();
+  }
+
+  loadModel(){
+  	this.paymentProgress = [
+  		{
+  			"name": "shipping",
+  			"active": true
+  		},
+  		{
+  			"name": "payment",
+  			"active": false
+  		},
+  		{
+  			"name": "done",
+  			"active": false
+  		}
+  	]
+  }
+
+  showBreedcrumb(){
+    this.breedcrumb = [
+      {
+        "name": "Home",
+        "url": "/",
+        "active": false
+      },
+      {
+        "name": "Cart",
+        "url": "/cart",
+        "active": false
+      },
+      {
+        "name": "Shipping & Payment",
+        "url": "#",
+        "active": true
+      }
+    ];
+  }
+
+  getMyCart(){
+    var userId = this.nowishopGlobal.getUserInfo().userId;
+    this.http.get(this.dataService.baseUrl + 'UserAccount/GetUserCardList/' + userId).subscribe(
+      res=>{
+        this.cartList = res['Data'];
+        this.nowishopGlobal.setInLocalStorage('user-cart', this.cartList);
+      }
+    )
   }
 
 }
